128660cd98f3c6a5febcb4f4b5a15ad5f871a60d
juan jose vilchez barzola <juan_vb_1998@hotmail.com>
2021-06-01 19:29:06 -0500

    Agregar archivos de proyecto.

---
+++ MVCAJAX/Controllers/ControllerStudent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain;
+using Service;
+using System.Web.Mvc;
+using MVCAJAX.Controllers;
+
+namespace MVCAJAX.Controllers
+{
+    public class ControllerStudent : Controller
+    {
+        private StudentService service = new StudentService();
+        //get:Student
+
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Address = c.studentAddress,
+                             Name = c.studentName
+                         }).ToList();
+            return View(model);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+           
+    }
+}
---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Domain;
+using Service;
+using MVCAJAX.Models;
+using System.Web;
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentService service = new StudentService();
+        //get:Student
+
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Address = c.studentAddress,
+                             Name = c.studentName
+                         }).ToList();
+            return View(model);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
